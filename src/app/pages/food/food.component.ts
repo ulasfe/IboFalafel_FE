ff2a4ecfd43cb7075f7f6c150f9ab1c50fa4f8ce
ulas_ <ulas.fe@gmail.com>
2026-02-22 19:54:38 +0100

    food page version 1

--- src/app/pages/food/food.component.ts
+++ src/app/pages/food/food.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule, DecimalPipe } from '@angular/common';
+
+interface MenuItem {
+  name: string;
+  description: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-food',
-  imports: [],
+  imports: [CommonModule, DecimalPipe],
   templateUrl: './food.component.html',
   styleUrl: './food.component.css'
 })
 export class FoodComponent {
+breakfastItems: MenuItem[] = [
+    { name: 'HUMMUS KLASYCZNY', price: 32.00, description: 'podawany z chrupiącym aromatycznym chlebkiem...' },
+    { name: 'BABA GHANOUSH', price: 34.00, description: 'podawana z chrupiącym aromatycznym chlebkiem...' },
+    { name: 'SHAKSHUKA', price: 36.00, description: 'z aromatycznym tofu i podawana z chrupiącym...' }
+  ];
 
+  extras: MenuItem[] = [
+    { name: 'FALAFEL (1 SZT.)', price: 4.00, description: '' },
+    { name: 'WEGAŃSKI SER FETA', price: 4.00, description: '' },
+    { name: 'DODATKOWY SOS', price: 3.00, description: '' }
+  ];
 }
