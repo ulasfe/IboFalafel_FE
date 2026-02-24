4c04d95b0abefb06d8dac3dc9babeb4e2e5376c2
ulas_ <ulas.fe@gmail.com>
2026-02-24 14:11:52 +0100

    Contact page almost finished

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [GoogleMapsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  phone = signal('+48795012876');
+  center: google.maps.LatLngLiteral = { lat: 51.11167867037646, lng: 17.02810998206718 };
+  zoom = 16;
+  googleReviewUrl = 'https://search.google.com/local/writereview?placeid=ChIJG52qJCzDD0cRD94WalGAhbQ';
+   
+  currentLang = signal<'pl' | 'en'>('pl');
+  markerPosition: google.maps.LatLngLiteral = { lat: 51.11167867037646, lng: 17.02810998206718 };
+  
+  options: google.maps.MapOptions = {
+    disableDefaultUI: true, 
+    zoomControl: true,
+    scrollwheel: false,
+    styles: [{ featureType: "landscape.natural", elementType: "geometry", stylers: [{ color: "#e8f5e9" }] },
+  { featureType: "poi.park", elementType: "geometry", stylers: [{ color: "#c8e6c9" }] },
+  { featureType: "water", elementType: "geometry", stylers: [{ color: "#a5d6a7" }] },
+  { featureType: "road", elementType: "geometry.fill", stylers: [{ color: "#ffffff" }] } ]
+  };
 }
